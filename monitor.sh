4b02886e3eedc91bc774c0245c2bf7c8b3cbb957
Ecem Erikli <efe7@njit.edu>
2026-05-16 15:49:35 -0400

    Create monitoring script

--- monitor.sh
+++ monitor.sh
@@ -3,4 +3,4 @@
 
 echo "Memory Log - $(date)" >> system_log.txt
 free -h | grep Mem >> system_log.txt
-echo "-------------" >> system_log.txt
+echo "--------------------------------" >> system_log.txt
